1e36d385c3ea329a1b7294cd17fe1f150e275ffd
Vandan Patel <vandan.ets@gmail.com>
2019-10-20 23:37:20 -0400

    Initial Commit

---
+++ iDine-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iDine-SwiftUI
+//
+//  Created by Vandan Patel on 10/20/19.
+//  Copyright © 2019 Vandan Patel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
